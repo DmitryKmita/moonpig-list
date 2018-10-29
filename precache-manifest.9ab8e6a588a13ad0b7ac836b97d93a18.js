28467c3d61559dfbaeae1f9c86a7385d3f3a3608
Dmitrijs Kmita <kmitad@C02V42UZHV2H.lan>
2018-10-29 23:31:37 +0000

    Updates

---
+++ precache-manifest.9ab8e6a588a13ad0b7ac836b97d93a18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d425cd0580f6d53aac98",
+    "url": "/moonpig-list/static/js/runtime~main.d425cd05.js"
+  },
+  {
+    "revision": "4fea4741a26c9fc6bb47",
+    "url": "/moonpig-list/static/js/main.4fea4741.chunk.js"
+  },
+  {
+    "revision": "33fda02578b0d4392f11",
+    "url": "/moonpig-list/static/js/1.33fda025.chunk.js"
+  },
+  {
+    "revision": "4fea4741a26c9fc6bb47",
+    "url": "/moonpig-list/static/css/main.ae6d49eb.chunk.css"
+  },
+  {
+    "revision": "a9616b0636a0edf1e1ecfc6fcf8e9db5",
+    "url": "/moonpig-list/index.html"
+  }
+];
